774fffd6cb36718422edc89514899781912570c4
PONSELVIK <PONSSELVI2@GMAIL.COM>
2022-09-08 00:42:30 +0530

    day1

---
+++ index.js
@@ -0,0 +1,57 @@
+// console.log("working");
+// //1.
+// let a=2;
+// let b="2";
+// console.log(a==b);//true
+// console.log(a===b);//false
+
+// //2.
+// let arr1=[1,2,3,4]
+// let arr2=[...arr1]
+// console.log(arr2);
+
+// var array1=["h","e","y"]
+// console.log(...array1);
+
+// let array4 = ['h', 'e', 'y'];
+// let array2 = [...array4];
+// console.log(array2);
+
+// //3.
+// 	let add = function (){
+//         let num=20;
+//         console.log(num);
+//     }
+//     add();
+
+//     var sub=()=>{
+//         x=10;
+//         y=20;
+//         console.log(y-x);
+//     }
+//     sub()
+
+//     const n=10;
+//     function f(){
+//         // console.log(n);
+//         const n=90;
+//     }
+//     console.log(n);
+    
+//     //5.
+//     var functionA = funtion(z){
+//         console.log("z");
+//     }
+//     function abc(){
+//         console.log("i am abc function");
+//     }
+//     functionA(abc);
+
+//     const functionB =function(){
+//         return funtion(){
+//             ("i am ab function");
+//     }
+//     let returnedfunction = function();
+//         console.log("i am returned function");
+//     }
+//    console.log(returnedfunction);
